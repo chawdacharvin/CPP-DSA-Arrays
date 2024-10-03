21e53427beffb380fcdc492a3cb25453da11c100
chawdacharvin <chawdacharvinj@gmail.com>
2024-10-03 19:12:34 +0530

    Commited first code for C++

---
+++ first.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;/*or we can write
+ function like "using std::cout" OR std:: string" etc*/
+ /* Namespace is used for including string and vector
+  files code and printing function is also incl.*/
+  int main(){
+      cout << "Namaste bhaio aur behno" << endl;
+      cout << "Namaste bhaio aur behno\n";
+      std::cout << "Namaste Bhaio aur Behno with std";
+
+      
+  }
