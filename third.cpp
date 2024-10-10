16f56148208a8d0aea05a33138da8d40294545de
chawdacharvin <chawdacharvinj@gmail.com>
2024-10-10 16:14:58 +0530

    commited 3 new files

---
+++ third.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main(){
+      char b =90;
+      int a = 2356712343;
+      ;/*ASCII table from 97 to 123 it is small alpha 
+      but from 65 to 91 it is capital alpha */
+      char c = 69;
+      std:: cout << char(c)<< "\n";
+      std:: cout << char(b);
+      std:: cout << char(a);
+}
