d0e70f8b9b42696d657ed80700b25d9be80ede6f
chawdacharvin <chawdacharvinj@gmail.com>
2024-10-10 15:55:54 +0530

    Commited

---
+++ fourth.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+
+int main(){
+      int a;
+      std::cout <<  "Please enter any number\n";
+      std::cin >> a;
+      if(a==0){
+            std::cout <<"It is Zero(0)";
+      }
+      else if (a<0)
+      {
+            std::cout <<"It is Negative";
+      }
+      else
+      {
+            std::cout<<"It is Positive";
+      }
+      
+      
+} 
